21a5dc8e9d56f5fff188838a6e33f0c945ddc3b7
Vasko1234 <vasilvelev07@gmail.com>
2023-05-21 01:59:16 +0300

    all player animations added

--- code/support.py
+++ code/support.py
@@ -1,7 +1,13 @@
 from os import walk
+import pygame
 
 def import_folder(path):
-    for informarion in walk(path):
-        print(informarion)
+    surface_list = []
 
-import_folder("../graphics/character/run")
+    for _, __, image_files in walk(path):
+        for image in image_files:
+            full_path = path + "/" + image
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list
