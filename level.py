b45c148c6a68a2f46d0e0bc55b03c2b0edf316ee
Vasko1234 <vasilvelev07@gmail.com>
2023-05-20 19:58:41 +0300

    "update" method added

--- level.py
+++ level.py
@@ -6,11 +6,12 @@ class Level:
     def __init__(self, level_map, surface):
         self.display_surface = surface
         self.setup_level(level_map)
+        self.world_shift = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         for row_index, row in enumerate(layout):
-            for col_index, cell in enumerate(layout):
+            for col_index, cell in enumerate(row):
                 if cell == "X":
                     x = col_index * tile_size
                     y = row_index * tile_size
@@ -18,4 +19,5 @@ class Level:
                     self.tiles.add(tile)
 
     def run(self):
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
--- tiles.py
+++ tiles.py
@@ -4,5 +4,8 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, position, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.image.fill((55, 55, 55))
+        self.image.fill((220, 220, 240))
         self.rect = self.image.get_rect(topleft = position)
+
+    def update(self, shift):
+        self.rect.x += shift
