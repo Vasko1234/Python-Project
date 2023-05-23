3f9375a754e9020cbc31a570cb55790e93c704f8
Vasko1234 <vasilvelev07@gmail.com>
2023-05-24 00:44:02 +0300

    tiles file created

--- code part 2/level.py
+++ code part 2/level.py
@@ -1,12 +1,27 @@
 import pygame
 from support import import_csv_layout
+from settings import tile_size
+from tiles import Tile
 
 class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
 
         terrain_layout = import_csv_layout(level_data["terrain"])
-        print(terrain_layout)
+        self.terrain_sprites = self.create_tile_group(terrain_layout, "terrain")
+
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index, value in enumerate(row):
+                if value != "-1":
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == "terrain":
+                        sprite = Tile(tile_size, x, y)
+                        sprite_group.add(sprite)
 
     def run(self):
         pass
