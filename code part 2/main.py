eff79530c310489d26347d2b85f256b12cd23a35
Vasko1234 <vasilvelev07@gmail.com>
2023-05-24 00:03:08 +0300

    next part of creating the game

---
+++ code part 2/main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+FPS = 240
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.set_caption(f"FPS: {int(clock.get_fps())}")
+    screen.fill((0, 0, 0))
+
+    pygame.display.update()
+    clock.tick(FPS)
