da9d43c0e6fafb4807cc9507ecd8ced96485e41f
Vasko1234 <vasilvelev07@gmail.com>
2023-05-25 01:40:06 +0300

    game_data file crated

---
+++ code part 3/game_data.py
@@ -0,0 +1,44 @@
+level_0 = {
+    "node_position": (110, 400),
+    "content": "this is level 0",
+    "unlock": 1
+}
+
+level_1 = {
+    "node_position": (300, 220),
+    "content": "this is level 1",
+    "unlock": 2
+}
+
+level_2 = {
+    "node_position": (480, 610),
+    "content": "this is level 2",
+    "unlock": 3
+}
+
+level_3 = {
+    "node_position": (610, 350),
+    "content": "this is level 3",
+    "unlock": 4
+}
+
+level_4 = {
+    "node_position": (880, 210),
+    "content": "this is level 4",
+    "unlock": 5
+}
+
+level_5 = {
+    "node_position": (1050, 400),
+    "content": "this is level 5",
+    "unlock": 5
+}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
+}
