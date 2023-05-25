eddf6098c5df22ab69421d8ad5dad805f33f093a
Vasko1234 <vasilvelev07@gmail.com>
2023-05-25 14:42:35 +0300

    connecting the overworld with the levels

--- code/game_data.py
+++ code/game_data.py
@@ -12,3 +12,87 @@ level_0 = {
     "unlock" : 1,
     "node_graphics": "Python-Project/graphics/green_graphics/overworld/0"
 }
+
+level_1 = {
+    "terrain": "Python-Project/levels/0/level_0._terrain.csv",
+    "coins": "Python-Project/levels/0/level_0._coins.csv",
+    "fg_palms": "Python-Project/levels/0/level_0._fg_palms.csv",
+    "bg_palms": "Python-Project/levels/0/level_0._bg_palms.csv",
+    "crates": "Python-Project/levels/0/level_0._crates.csv",
+    "enemies": "Python-Project/levels/0/level_0._enemies.csv",
+    "constraints": "Python-Project/levels/0/level_0._constraints.csv",
+    "player": "Python-Project/levels/0/level_0._player.csv",
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (300, 220),
+    "unlock" : 2,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/1"
+}
+
+level_2 = {
+    "terrain": "Python-Project/levels/0/level_0._terrain.csv",
+    "coins": "Python-Project/levels/0/level_0._coins.csv",
+    "fg_palms": "Python-Project/levels/0/level_0._fg_palms.csv",
+    "bg_palms": "Python-Project/levels/0/level_0._bg_palms.csv",
+    "crates": "Python-Project/levels/0/level_0._crates.csv",
+    "enemies": "Python-Project/levels/0/level_0._enemies.csv",
+    "constraints": "Python-Project/levels/0/level_0._constraints.csv",
+    "player": "Python-Project/levels/0/level_0._player.csv",
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (480, 610),
+    "unlock" : 3,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/2"
+}
+
+level_3 = {
+    "terrain": "Python-Project/levels/0/level_0._terrain.csv",
+    "coins": "Python-Project/levels/0/level_0._coins.csv",
+    "fg_palms": "Python-Project/levels/0/level_0._fg_palms.csv",
+    "bg_palms": "Python-Project/levels/0/level_0._bg_palms.csv",
+    "crates": "Python-Project/levels/0/level_0._crates.csv",
+    "enemies": "Python-Project/levels/0/level_0._enemies.csv",
+    "constraints": "Python-Project/levels/0/level_0._constraints.csv",
+    "player": "Python-Project/levels/0/level_0._player.csv",
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (610, 350),
+    "unlock" : 4,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/3"
+}
+
+level_4 = {
+    "terrain": "Python-Project/levels/0/level_0._terrain.csv",
+    "coins": "Python-Project/levels/0/level_0._coins.csv",
+    "fg_palms": "Python-Project/levels/0/level_0._fg_palms.csv",
+    "bg_palms": "Python-Project/levels/0/level_0._bg_palms.csv",
+    "crates": "Python-Project/levels/0/level_0._crates.csv",
+    "enemies": "Python-Project/levels/0/level_0._enemies.csv",
+    "constraints": "Python-Project/levels/0/level_0._constraints.csv",
+    "player": "Python-Project/levels/0/level_0._player.csv",
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (880, 210),
+    "unlock" : 5,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/4"
+}
+
+level_5 = {
+    "terrain": "Python-Project/levels/0/level_0._terrain.csv",
+    "coins": "Python-Project/levels/0/level_0._coins.csv",
+    "fg_palms": "Python-Project/levels/0/level_0._fg_palms.csv",
+    "bg_palms": "Python-Project/levels/0/level_0._bg_palms.csv",
+    "crates": "Python-Project/levels/0/level_0._crates.csv",
+    "enemies": "Python-Project/levels/0/level_0._enemies.csv",
+    "constraints": "Python-Project/levels/0/level_0._constraints.csv",
+    "player": "Python-Project/levels/0/level_0._player.csv",
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (1050, 400),
+    "unlock" : 5,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/5"
+}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
+}
--- code/main.py
+++ code/main.py
@@ -2,14 +2,36 @@ import pygame
 import sys
 from settings import *
 from level import Level
-from game_data import level_0
+from overworld import Overworld
+
+class Game:
+    def __init__(self):
+        self.max_level = 2
+        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+        self.status = "overworld"
+
+    def create_level(self, current_level):
+        self.level = Level(current_level, screen, self.create_overworld)
+        self.status = "level"
+
+    def create_overworld(self, current_level, new_max_level): 
+        if new_max_level > self.max_level:
+            self.max_level = new_max_level
+        self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
+        self.status = "overworld"
+
+    def run(self):
+        if self.status == "overworld":
+            self.overworld.run()
+        else:
+            self.level.run()
 
 pygame.init()
 
 FPS = 240
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-level = Level(level_0, screen)
+game = Game()
 
 while True:
     for event in pygame.event.get():
@@ -19,7 +41,7 @@ while True:
 
     pygame.display.set_caption(f"FPS: {int(clock.get_fps())}")
     screen.fill((0, 0, 0))
-    level.run()
+    game.run()
 
     pygame.display.update()
     clock.tick(FPS)
