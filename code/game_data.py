938d1c69409575014c1ae25dea0c5d7879efb95d
Vasko1234 <vasilvelev07@gmail.com>
2023-05-25 14:24:53 +0300

    overworld graphics added

--- code/game_data.py
+++ code/game_data.py
@@ -7,5 +7,8 @@ level_0 = {
     "enemies": "Python-Project/levels/0/level_0._enemies.csv",
     "constraints": "Python-Project/levels/0/level_0._constraints.csv",
     "player": "Python-Project/levels/0/level_0._player.csv",
-    "grass": "Python-Project/levels/0/level_0._grass.csv"
+    "grass": "Python-Project/levels/0/level_0._grass.csv",
+    "node_position": (110, 400),
+    "unlock" : 1,
+    "node_graphics": "Python-Project/graphics/green_graphics/overworld/0"
 }
