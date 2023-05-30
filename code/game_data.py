5ba3b2ed3eac1181d69287fa3aeac18fad0cb015
Vasko1234 <vasilvelev07@gmail.com>
2023-05-31 01:32:47 +0300

    level 2 created

--- code/game_data.py
+++ code/game_data.py
@@ -16,15 +16,15 @@ level_0 = {
 }
 
 level_1 = {
-    "terrain": "Python-Project/levels/0/level_0_terrain.csv",
-    "coins": "Python-Project/levels/0/level_0_coins.csv",
-    "fg_palms": "Python-Project/levels/0/level_0_fg_palms.csv",
-    "bg_palms": "Python-Project/levels/0/level_0_bg_palms.csv",
-    "crates": "Python-Project/levels/0/level_0_crates.csv",
-    "enemies": "Python-Project/levels/0/level_0_enemies.csv",
-    "constraints": "Python-Project/levels/0/level_0_constraints.csv",
-    "player": "Python-Project/levels/0/level_0_player.csv",
-    "grass": "Python-Project/levels/0/level_0_grass.csv",
+    "terrain": "Python-Project/levels/1/level_1_tarrain.csv",
+    "coins": "Python-Project/levels/1/level_1_coins.csv",
+    "fg_palms": "Python-Project/levels/1/level_1_fg_palms.csv",
+    "bg_palms": "Python-Project/levels/1/level_1_bg_palms.csv",
+    "crates": "Python-Project/levels/1/level_1_crates.csv",
+    "enemies": "Python-Project/levels/1/level_1_enemies.csv",
+    "constraints": "Python-Project/levels/1/level_1_constraints.csv",
+    "player": "Python-Project/levels/1/level_1_player.csv",
+    "grass": "Python-Project/levels/1/level_1_grass.csv",
     "node_position": (300, 220),
     "unlock" : 2,
     "node_graphics": f"Python-Project/graphics/{graphics_color}_graphics/overworld/1"
